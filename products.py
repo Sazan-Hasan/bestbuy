8882c9642b9646e5c99b72c87fce1ede3d0f6d66
Sazan Hasan <Sazan.HasanHaji@gmail.com>
2026-03-12 14:22:55 +0100

    create class product

---
+++ products.py
@@ -0,0 +1,61 @@
+class Product:
+
+    def __init__(self, name, price, quantity):
+        if not name or price < 0 or quantity < 0:
+            raise Exception("Invalid product values")
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        if quantity < 0:
+            raise Exception("Quantity cannot be negative")
+
+        self.quantity = quantity
+
+        if self.quantity == 0:
+            self.deactivate()
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+    def buy(self, quantity):
+        if quantity <= 0:
+            raise Exception("Invalid purchase quantity")
+
+        if quantity > self.quantity:
+            raise Exception("Not enough items in stock")
+
+        self.quantity -= quantity
+
+        if self.quantity == 0:
+            self.deactivate()
+
+        return quantity * self.price
+
+bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+print(bose.buy(50))
+print(mac.buy(100))
+print(mac.is_active())
+
+bose.show()
+mac.show()
+
+bose.set_quantity(1000)
+bose.show()
